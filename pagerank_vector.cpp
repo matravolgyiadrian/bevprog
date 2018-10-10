a845d2e04c35a252830bbf0a642955852bbf4fa1
matravolgyiadrian <43448141+matravolgyiadrian@users.noreply.github.com>
2018-10-10 19:14:34 +0200

    Add files via upload

---
+++ pagerank_vector.cpp
@@ -0,0 +1,47 @@
+#include "std_lib_facilities.h"
+void kiir (vector<double> vektor, int db)
+{
+	int i;
+	for(i=0; i<db; i++)
+		cout<<"PageRank["<< i <<"]:\t" <<vektor[i] <<'\n';
+}
+
+double tavolsag(vector<double> PR, vector<double> PRv,int n)
+{
+	double osszeg = 0.0;
+	int i;
+	for(i=0;i<n;i++)
+		osszeg +=(PRv[i] - PR[i])*(PRv[i] - PR[i]);
+	return sqrt(osszeg);
+}
+
+int main()
+{
+	vector<vector<double>> L = {
+		{0.0, 0.0, 1.0 / 3.0, 0.0},
+		{1.0, 1.0 / 2.0, 1.0 / 3.0, 1.0},
+		{0.0, 1.0 / 2.0, 0.0, 0.0},
+		{0.0, 0.0, 1.0 / 3.0, 0.0}
+	};
+
+	vector<double> PR = {0.0, 0.0, 0.0, 0.0};
+	vector<double> PRv = {1.0 / 4.0, 1.0 / 4.0, 1.0 / 4.0, 1.0 / 4.0};
+
+	int i,j;
+
+	for(;;)
+	{
+		for(i=0; i<4; ++i)
+		{
+			PR[i]=0.0;
+			for(j=0; j<4; ++j)
+				PR[i]+=L[i][j]*PRv[j];
+		}
+		if ( tavolsag(PR,PRv, 4) < 0.0000000001)
+			break;
+		for(i=0; i<4; ++i)
+			PRv[i]=PR[i];
+	}
+	kiir (PR,4);
+	return 0;
+} 
